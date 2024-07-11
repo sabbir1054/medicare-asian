7fd4bbab395c1762b58ed0a1c71cbe83c9e9662e
sabbir1054 <mdsabbir1054@gmail.com>
2024-07-11 10:06:37 +0600

    impliment- login page

--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { Button, Form, Input } from "antd";
-
+type ILoginInput = {
+  emai: string;
+  password: string;
+};
 const LoginForm = () => {
-  const onFinish = (values: any) => {
+  const onFinish = (values: ILoginInput) => {
     console.log("Success:", values);
   };
 
@@ -18,27 +21,66 @@ const LoginForm = () => {
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
+      style={{
+        width: "100%",
+        maxWidth: "400px",
+        padding: "40px",
+        borderRadius: "10px",
+      }}
+      className="bg-white  rounded-lg shadow-lg "
     >
+      <h1 className="text-2xl text-left font-secondary font-semibold mb-3">
+        Login
+      </h1>
       <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please input your username!" }]}
+        name="email"
+        rules={[{ required: true, message: "Please input your email!" }]}
+        className="mb-6"
       >
-        <Input />
+        <Input
+          placeholder="name@company.com"
+          className="bg-gray-100 border border-gray-300 text-gray-900 rounded-lg py-2 px-4 w-full"
+        />
       </Form.Item>
 
       <Form.Item
-        label="Password"
         name="password"
         rules={[{ required: true, message: "Please input your password!" }]}
+        className="mb-6"
       >
-        <Input.Password />
+        <Input.Password
+          placeholder="Password"
+          className="bg-gray-100 border border-gray-300 text-gray-900 rounded-lg py-2 px-4 w-full"
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        className="flex items-center justify-between mb-4"
+      >
+        <a href="" className="text-primary">
+          Forgot password?
+        </a>
+      </Form.Item>
+
+      <Form.Item className="mb-6">
+        <Button
+          // style={{ backgroundColor: "var(--primaryColor) !important" }}
+          htmlType="submit"
+          className="!bg-primary !text-white py-2 px-4 w-full rounded-lg"
+        >
+          Sign in
+        </Button>
       </Form.Item>
 
       <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
+        <div className="text-center text-gray-900">
+          Do not have an account yet?
+          <a href="" className="text-primary">
+            Sign up
+          </a>
+        </div>
       </Form.Item>
     </Form>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#0e7673",
+        secondary: "#e8292f",
+      },
+      fontFamily: {
+        primary: ["Poppins", "sans-serif"],
+        secondary: ["Montserrat", "sans-serif"],
       },
     },
   },
